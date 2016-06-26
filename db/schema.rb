5875ec5aa4aabe9c8c08ad2729740ac1a721b17c
Edgar Quintana <edgarquintana984@hotmail.com>
2016-06-26 07:17:10 -0500

    Crea Model Answer con asociacion polimorfica hacia Comment_Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626115821) do
+ActiveRecord::Schema.define(version: 20160626121554) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "answers", ["user_id"], name: "index_answers_on_user_id"
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
